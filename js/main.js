f0f92c052beee8ac887fde4fa34768b12344d9a1
Adarshk33 <adarsh.sinha33@gmail.com>
2025-10-21 16:57:44 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -232,7 +232,7 @@ $(document).ready(function(){
                     zoom: 11,
 
                     // The latitude and longitude to center the map (always required)
-                    center: new google.maps.LatLng(40.6700, -73.9400), // New York
+                    center: new google.maps.LatLng(28.644800,  77.216721), //delhi
 
                     // How you would like to style the map. 
                     // This is where you would paste any style found on Snazzy Maps.
